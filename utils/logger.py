1ea9fb6735b35b79d482797c4f2ef30b0ed5bec5
Pavel Miskevich <chiki-takipavel@mail.ru>
2023-04-24 09:57:11 +0300

    Add logger

---
+++ utils/logger.py
@@ -0,0 +1,16 @@
+import logging
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+
+    return logger
