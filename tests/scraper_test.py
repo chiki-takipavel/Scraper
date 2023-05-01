53fda9cdce2d3f6b8407d66cd4198a9c6b170989
Pavel Miskevich <chiki-takipavel@mail.ru>
2023-05-01 20:20:24 +0300

    Add tests for scraper and statistics

---
+++ tests/scraper_test.py
@@ -0,0 +1,99 @@
+import unittest
+from bs4 import BeautifulSoup
+from scraper import (
+    get_title, get_original_title, get_description, get_genres, get_duration,
+    get_countries, get_premier_date, get_imdb_rate, get_kinopoisk_rate
+)
+
+
+class TestScraper(unittest.TestCase):
+    def setUp(self):
+        html = """
+            <html>
+                <body>
+                    <div class="b-post__title"> 
+                        <h1 itemprop="name">Тестовый фильм</h1>
+                    </div>
+                    <div class="b-post__origtitle" itemprop="alternativeHeadline">Тестовый фильм / Test movie</div>
+                    <table class="b-post__info">
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <span class="b-post__info_rates imdb">
+                                        <span class="bold">8.2</span>
+                                    </span>
+                                    <span class="b-post__info_rates kp">
+                                        <span class="bold">7.71</span>
+                                    </span>
+                                </td>
+                            <tr>
+                                <td>8 сентября <a href="https://hdrezka.co/year/2013/">2013 года</a></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="https://hdrezka.co/country/USA/">США</a>, 
+                                    <a href="https://hdrezka.co/country/UK/">Великобритания</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="https://hdrezka.co/films/drama/"><span itemprop="genre">Драмы</span></a>, 
+                                    <a href="https://hdrezka.co/films/thriller/"><span itemprop="genre">Триллеры</span></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td itemprop="duration">120 мин.</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div class="b-post__description_text">Описание тестового фильма.</div>
+                </body>
+            </html>
+            """
+
+        self.soup = BeautifulSoup(html, "html.parser")
+
+    def test_get_title(self):
+        result = get_title(self.soup)
+
+        self.assertEqual(result, "Тестовый фильм")
+
+    def test_get_original_title(self):
+        result = get_original_title(self.soup)
+
+        self.assertEqual(result, "Тестовый фильм / Test movie")
+
+    def test_get_description(self):
+        result = get_description(self.soup)
+
+        self.assertEqual(result, "Описание тестового фильма.")
+
+    def test_get_genres(self):
+        result = get_genres(self.soup)
+
+        self.assertEqual(result, ["Драмы", "Триллеры"])
+
+    def test_get_duration(self):
+        result = get_duration(self.soup)
+
+        self.assertEqual(result, "120 мин.")
+
+    def test_get_countries(self):
+        result = get_countries(self.soup)
+
+        self.assertEqual(result, ["США", "Великобритания"])
+
+    def test_get_premier_date(self):
+        result = get_premier_date(self.soup)
+
+        self.assertEqual(result, ("2013-09-08", "2013"))
+
+    def test_get_imdb_rate(self):
+        result = get_imdb_rate(self.soup)
+
+        self.assertEqual(result, "8.2")
+
+    def test_get_kinopoisk_rate(self):
+        result = get_kinopoisk_rate(self.soup)
+
+        self.assertEqual(result, "7.71")
---
+++ tests/statistics_test.py
@@ -0,0 +1,74 @@
+import unittest
+from statistics import (
+    get_season, get_rate_group, count_movies_by_season,
+    count_movies_by_country, count_movies_by_genre
+)
+from utils.constants import Constants
+from datetime import date
+
+
+class TestStatistics(unittest.TestCase):
+    def test_get_season(self):
+        self.assertEqual(get_season(date(2023, 1, 1)), Constants.WINTER)
+        self.assertEqual(get_season(date(2023, 4, 1)), Constants.SPRING)
+        self.assertEqual(get_season(date(2023, 7, 1)), Constants.SUMMER)
+        self.assertEqual(get_season(date(2023, 11, 1)), Constants.AUTUMN)
+
+    def test_get_rate_group(self):
+        self.assertEqual(get_rate_group(0), Constants.RATE_GROUP_0_2)
+        self.assertEqual(get_rate_group(1.5), Constants.RATE_GROUP_0_2)
+        self.assertEqual(get_rate_group(2), Constants.RATE_GROUP_0_2)
+        self.assertEqual(get_rate_group(3.2), Constants.RATE_GROUP_2_4)
+        self.assertEqual(get_rate_group(4), Constants.RATE_GROUP_2_4)
+        self.assertEqual(get_rate_group(5.7), Constants.RATE_GROUP_4_6)
+        self.assertEqual(get_rate_group(6), Constants.RATE_GROUP_4_6)
+        self.assertEqual(get_rate_group(7.5), Constants.RATE_GROUP_6_8)
+        self.assertEqual(get_rate_group(8), Constants.RATE_GROUP_6_8)
+        self.assertEqual(get_rate_group(9.2), Constants.RATE_GROUP_8_10)
+        self.assertEqual(get_rate_group(10), Constants.RATE_GROUP_8_10)
+
+    def test_count_movies_by_season(self):
+        movies = [
+            {"premiere_date": "2023-01-01"},
+            {"premiere_date": "2023-04-01"},
+            {"premiere_date": "2023-07-01"},
+            {"premiere_date": "2023-11-01"},
+            {"premiere_date": None},
+        ]
+        expected_counts = {Constants.WINTER: 1, Constants.SPRING: 1, Constants.SUMMER: 1, Constants.AUTUMN: 1}
+
+        result = count_movies_by_season(movies)
+
+        self.assertEqual(result, expected_counts)
+
+    def test_count_movies_by_country(self):
+        movies = [
+            {"countries": ["США"]},
+            {"countries": ["США", "Франция"]},
+            {"countries": ["Франция"]},
+            {"countries": ["Германия", "Франция", "США"]},
+            {"countries": ["Россия"]},
+        ]
+        expected_counts = {"США": 3, "Франция": 3, "Германия": 1, "Россия": 1}
+
+        result = count_movies_by_country(movies)
+
+        self.assertEqual(result, expected_counts)
+
+    def test_count_movies_by_genre(self):
+        movies = [
+            {"genres": ["Драмы"]},
+            {"genres": ["Драмы", "Боевики", "Зарубежные"]},
+            {"genres": ["Комедии"]},
+            {"genres": ["Драмы", "Русские"]},
+            {"genres": ["Триллеры"]},
+        ]
+        expected_counts = {"Драмы": 3, "Боевики": 1, "Комедии": 1, "Триллеры": 1}
+
+        result = count_movies_by_genre(movies)
+
+        self.assertEqual(result, expected_counts)
+
+
+if __name__ == '__main__':
+    unittest.main()
